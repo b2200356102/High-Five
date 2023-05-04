1f1fc1dc88cf157faa4ca897b23336deeb13030e
b2200356102 <b2200356102@cs.hacettepe.edu.tr>
2023-05-04 12:35:28 +0300

    Add Course modified

--- Frontend/src/pages/AddCourse.js
+++ Frontend/src/pages/AddCourse.js
@@ -2,14 +2,20 @@ import React, { useState } from "react";
 import "../styles/AddCourse.css";
 
 function AddCourse() {
-    const [courseIdName, setCourseIdName] = useState("");
+    const [courseId, setCourseId] = useState("");
+    const [courseName, setCourseName] = useState("");
     const [department, setDepartment] = useState("");
     const [credit, setCredit] = useState("");
+    const [capacity, setCapacity] = useState("");
     const [courseType, setCourseType] = useState("");
     const [education, setEducation] = useState("");
 
-    const handleCourseIdNameChange = (e) => {
-        setCourseIdName(e.target.value);
+    const handleCourseIdChange = (e) => {
+        setCourseId(e.target.value);
+    };
+
+    const handleCourseNameChange = (e) => {
+        setCourseName(e.target.value);
     };
 
     const handleDepartmentChange = (e) => {
@@ -18,6 +24,10 @@ function AddCourse() {
 
     const handleCreditChange = (e) => {
         setCredit(e.target.value);
+    };
+
+    const handleCapacityChange = (e) => {
+        setCapacity(e.target.value);
     };
 
     const handleCourseTypeChange = (e) => {
@@ -30,18 +40,50 @@ function AddCourse() {
 
 
     const handleButtonClick = () => {
-        /* */
 
+        const data = { courseId, courseName, department, credit,capacity, courseType, education };
+        /*
+        fetch('/api/courses', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(data)
+        })
+        .then(response => {
+          if (response.ok) {
+            console.log('Course added successfully');
+          } else {
+            console.error('Error adding course');
+          }
+        })
+        .catch(error => {
+          console.error('Error adding course', error);
+        });*/
+
+        console.log("Course ID - Course Name: ", courseId, " - ", courseName);
+        console.log("Department: ", department);
+        console.log("Credit: ", credit);
+        console.log("Capacity: ", capacity);
+        console.log("Course Type: ", courseType);
+        console.log("Education: ", education);
     };
+
     return (
         <div className="pageContainer"> {/* Parent container */}
             <div className="rectangle"> {/* Rectangle in the middle */}
                 {/* Content inside the rectangle */}
                 <div className='slotrectangle'>
-                    <p className='mainText'>Course ID - Course Name:</p>
+                    <p className='mainText'>Course ID:</p>
                     <div className='fieldrectangle'>
                         {/* Text field for Course ID - Course Name */}
-                        <input type="text" id="courseidname" value={courseIdName} onChange={handleCourseIdNameChange} />
+                        <input type="text" id="courseid" value={courseId} onChange={handleCourseIdChange} />
+                    </div>
+                </div>
+
+                <div className='slotrectangle'>
+                    <p className='mainText'>Course Name:</p>
+                    <div className='fieldrectangle'>
+                        {/* Text field for Course ID - Course Name */}
+                        <input type="text" id="coursename" value={courseName} onChange={handleCourseNameChange} />
                     </div>
                 </div>
 
@@ -58,6 +100,14 @@ function AddCourse() {
                     <div className='fieldrectangle'>
                         {/* Text field for Credit */}
                         <input type="text" id="credit" value={credit} onChange={handleCreditChange} />
+                    </div>
+                </div>
+
+                <div className='slotrectangle'>
+                    <p className='mainText'>Course Capacity:</p>
+                    <div className='fieldrectangle'>
+                        {/* Text field for Credit */}
+                        <input type="text" id="capacity" value={capacity} onChange={handleCapacityChange} />
                     </div>
                 </div>
 
